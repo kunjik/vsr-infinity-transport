0275b39d9537dcd709a3fec4243e603826537dbf
Shantanu Pandey <imshantanupandey@gmail.com>
2026-06-10 23:02:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,17 @@ navLinks.forEach((link) => {
   });
 });
 
+let topButton = document.getElementById("backToTopBtn");
+
+// Scroll hone par ye function chalega
+window.onscroll = function() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    topButton.style.display = "block"; // Button dikhao
+  } else {
+    topButton.style.display = "none";  // Button chupao
+  }
+};
+
 quoteForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
